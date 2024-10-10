f2c80fa634c3b4deb176aa99c9fcd5991755e951
Miiiiroooo <84608805+Miiiiroooo@users.noreply.github.com>
2024-10-10 14:32:22 +0800

    [incomplete] created camera class

--- GDENG03-Activities/Code/GameEngine/Components/Camera/Camera.cpp
+++ GDENG03-Activities/Code/GameEngine/Components/Camera/Camera.cpp
@@ -1,1 +1,63 @@
 #include "Camera.h"
+#include "../Transform.h"
+
+
+Camera::Camera(int width, int height) : AComponent("Camera", EComponentTypes::Camera)
+{
+	ProjectionType = EProjectionTypes::Perspective;
+	AspectRatio = Vector2(width, height);
+	FieldOfView = 60.f;
+	clippingPlane = Vector2(0.1f, 1000.f);
+}
+
+Camera::Camera(std::string name, int width, int height) : AComponent(name, EComponentTypes::Camera)
+{
+	ProjectionType = EProjectionTypes::Perspective; 
+	AspectRatio = Vector2(width, height); 
+	FieldOfView = 60.f; 
+	clippingPlane = Vector2(0.1f, 1000.f); 
+}
+
+Camera::~Camera()
+{
+
+}
+
+void Camera::Perform()
+{
+
+}
+
+VPMatrix Camera::CreateViewProjectionMatrix()
+{
+	if (transform == nullptr) return VPMatrix{};
+
+	// https://gamedev.stackexchange.com/questions/178643/the-view-matrix-finally-explained
+	Matrix view = Matrix::CreateLookAt(transform->Position, transform->Position + transform->GetLocalForward(), transform->GetLocalUp());
+
+	Matrix projection = Matrix::Identity;
+	if (ProjectionType == EProjectionTypes::Orthographic)
+	{
+		projection = Matrix::CreateOrthographic(AspectRatio.x, AspectRatio.y, clippingPlane.x, clippingPlane.y);
+	}
+	else if (ProjectionType == EProjectionTypes::Perspective)
+	{
+		//projection = Matrix::CreatePerspective(AspectRatio.x, AspectRatio.y, clippingPlane.x, clippingPlane.y);
+		projection = Matrix::CreatePerspectiveFieldOfView(DirectX::XMConvertToRadians(FieldOfView), AspectRatio.x / AspectRatio.y, clippingPlane.x, clippingPlane.y);
+	}
+
+	view.Transpose();
+	projection.Transpose();
+
+	return VPMatrix{ view, projection };
+}
+
+Vector2 Camera::GetClippingPlane()
+{
+	return clippingPlane;
+}
+
+void Camera::SetClippingPlane(float nearZ, float farZ)
+{
+	clippingPlane = Vector2(nearZ, farZ);
+}
--- GDENG03-Activities/Code/GameEngine/Components/Camera/Camera.h
+++ GDENG03-Activities/Code/GameEngine/Components/Camera/Camera.h
@@ -1,6 +1,36 @@
 #pragma once
+#include <SimpleMath.h>
+#include "../AComponent.h"
+#include "EProjectionTypes.h"
+#include "../../../DirectXClasses/Buffers/BufferDataTypes.h"
+#include "../../../DirectXClasses/SwapChain.h"
 
-class Camera 
+
+using namespace DirectX::SimpleMath;
+
+class Camera : public AComponent
 {
+public:
+	Camera(int width, int height);
+	Camera(std::string name, int width, int height);
+	~Camera();
 
+	void Perform() override;
+	VPMatrix CreateViewProjectionMatrix();
+
+	Vector2 GetClippingPlane();
+	void SetClippingPlane(float nearZ, float farZ);
+	__declspec(property(get = GetClippingPlane, put = SetClippingPlane)) Vector2 ClippingPlane;
+
+
+public:
+	EProjectionTypes ProjectionType;
+	Vector2 AspectRatio;
+	float FieldOfView;
+
+private:
+	Vector2 clippingPlane;
+
+	//SwapChain* swapChain;
+	//Color bgColor;
 };
